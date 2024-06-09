8a135bec0031372d198843b532a9e4b6c4e223d0
Shubhrajyotidas <67722215+Shubhrajyotidas@users.noreply.github.com>
2024-06-09 07:06:32 +0000

    upto step 4

--- StringCalculator.java
+++ StringCalculator.java
@@ -9,8 +9,14 @@ public class StringCalculator {
         }
         String delimiter = ",";
         String numStr = numbers;
+        if (numbers.startsWith("//")) {
+            int delimiterEnd = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterEnd);
+            numStr = numbers.substring(delimiterEnd + 1);
+        }
         numStr = numStr.replace("\n", delimiter);
         String[] numArray = numStr.split(delimiter);
+        
 
        
         int sum = 0;
@@ -32,8 +38,8 @@ public class StringCalculator {
         System.out.println(calculator.add("")); // Output: 0
         System.out.println(calculator.add("1")); // Output: 1
         System.out.println(calculator.add("1,5")); // Output: 6
-        // System.out.println(calculator.add("1\n2,3")); // Output: 6
-        // System.out.println(calculator.add("//;\n1;2")); // Output: 3
+        System.out.println(calculator.add("1\n2,3")); // Output: 6
+        System.out.println(calculator.add("//;\n1;2")); // Output: 3
 
         // try {
         //     System.out.println(calculator.add("1,-2,3"));
